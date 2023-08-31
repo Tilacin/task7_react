b10bcef84cfe5f87e68699e759f6874113e70602
Tilacin <tilacin45@gmail.com>
2023-08-31 18:03:59 +0300

    Написали стартавую страницу

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispath] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispath({ type: "dataReceived", payload: data }))
+      .catch((err) => dispath({ type: "dataFailed" }));
   }, []);
 
   return (
@@ -15,12 +45,13 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
 }
-
+console.log(initialState)
 export default App;
 //191 7:50
