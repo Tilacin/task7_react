fe1a60aab073f13d028935ca7ede9f1702cc2f7a
Tilacin <tilacin45@gmail.com>
2023-09-01 20:34:10 +0300

    Реализовали почти всю логику, кроме кнопки начать с начала

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,18 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -28,15 +34,40 @@ function reducer(state, action) {
       };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action unkonwn");
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispath] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore }, dispath] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -52,12 +83,42 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispath}/>}
-        {status === "active" && <Question question={questions[index]}/>}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispath} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispath}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispath}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
+        )}
       </Main>
     </div>
   );
 }
 
 export default App;
-//195
+//199
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,15 +1,26 @@
-import React from 'react'
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
 
-function Options({question}) {
   return (
     <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
-  )
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default Options
+export default Options;
